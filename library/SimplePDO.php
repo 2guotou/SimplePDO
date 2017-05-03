795d42b8ba4bf41a46d1b29a07ed922f33df9c04
NuttyCase <qiuren.821@gmail.com>
2017-05-03 11:54:14 +0800

    Update SimplePDO.php
    remove yaf config depending

--- library/SimplePDO.php
+++ library/SimplePDO.php
@@ -1,37 +1,31 @@
 <?php
 /**
-    Simple PDO 是基于 PDO 类封装的 易用PDO 类
-
-    主要用于完成一些常规操作：
-        ::instance();         //获得一个PDO单例
-        ->doQuery();          //执行一段SQL
-        ->doPrepareExecute(); //执行一段预加载的SQL
-        ->doInsert();         //插入一条数据
-        ->doUpdate();         //更新一条数据
-
-    @author 刘东阳
-    @wrote  2015 spring
-*/
+ *  SimplePDO 是基于 PDO 类封装的 易用PDO 类
+ *  主要用于完成一些常规操作：
+ *      ::instance();         //获得一个PDO单例
+ *      ->doQuery();          //执行一段SQL
+ *      ->doPrepareExecute(); //执行一段预加载的SQL
+ *      ->doInsert();         //插入一条数据
+ *      ->doUpdate();         //更新一条数据
+ *  @author 刘东阳
+ *  @wrote  2015 spring
+ *  @repo   https://github.com/2guotou/SimplePDO
+ */
 class SimplePDO extends PDO{
     
     private static $pdo;
 
-    function __construct(){
-        $config = Yaf_Application::app()->getConfig();
-        parent::__construct(
-            $config->database->dsn, 
-            $config->database->user,
-            $config->database->pass,
-            array(
+    function __construct($dsn, $user, $pass){
+        parent::__construct($dsn,$user,$pass,[
                 //PDO::ATTR_PERSISTENT => TRUE, //keep persistent connect
                 PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8';",// charset
-            )
-        );
+        ]);
+        self::$pdo = $this;
     }
 
     /**
-        获取一个单例对象
-    */
+     * 获取一个单例对象
+     */
     static function &instance(){
         if( !(self::$pdo instanceof PDO) ){
             self::$pdo = new SimplePDO;
@@ -40,8 +34,8 @@ class SimplePDO extends PDO{
     }
 
     /**
-        完成 Query 操作，并依据 mode 返回相应的数据
-    */
+     * doQuery 完成 Query 操作，并依据 mode 返回相应的数据
+     */
     public function doQuery($sql, $mode='fetchAll'){
         $stmt = $this->query($sql);
         $stmt or self::throwException("Prepare Return Empty");
@@ -55,8 +49,8 @@ class SimplePDO extends PDO{
     }
 
     /**
-        完成 Prepare & Execute 系列操作，并根据 mode 返回相应的数据
-    */
+     * doPrepareExecute 完成 Prepare & Execute 系列操作，并根据 mode 返回相应的数据
+     */
     public function doPrepareExecute($prepare, $values, $mode='fetchAll'){
         $stmt = $this->prepare($prepare);
         $stmt or self::throwException("Prepare Return Empty");
@@ -71,8 +65,8 @@ class SimplePDO extends PDO{
     }
 
     /**
-        完成插入操作
-    */
+     * doInsert 完成插入操作
+     */
     public function doInsert($table, array $insert, array $update=array()){
         $fields  = implode('`, `', array_keys($insert));
         $holder  = implode(',',array_pad([], count($insert), '?'));
@@ -92,9 +86,9 @@ class SimplePDO extends PDO{
     }
 
     /**
-        完成更新操作
-        $where 和 $whereValues 有待合并简化
-    */
+     * doUpdate 完成更新操作
+     * $where 和 $whereValues 有待合并简化
+     */
     public function doUpdate($table, $update, $where='', array $whereValues=array()){
         $fields_list = [];
         foreach($update as $k => $v){
@@ -116,11 +110,10 @@ class SimplePDO extends PDO{
     }
 
     /**
-        方便程序当行内抛出异常
-        Sample：$cond or SimplePDO::throwExecption($msg);
-    */
+     * throwException 方便程序当行内抛出异常
+     * Sample：$cond or SimplePDO::throwExecption($msg);
+     */
     public static function throwException($msg, $code=1){
         throw new PDOException($msg, $code);
     }
 }
-?>
